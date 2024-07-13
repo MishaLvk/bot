f53d28960f0c2e43815abbbb8f01ece9aa63b020
Misha Levko <lvk.misha@gmail.com>
2024-07-14 02:26:41 +0300

    fixed koment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -106,19 +106,19 @@ const App = () => {
     }
   };
 
-  const handleBuy = async price => {
-    //const quantity = calculateQuantity(price);
-    await console.log(`купив по ${price}`);
-    setBuyPrice(price);
-    setMinPrice(null); // Reset the minPrice after buying
-  };
-
   // const handleBuy = async price => {
-  //   const quantity = calculateQuantity(price);
-  //   await placeOrder('BUY', price, quantity);
+  //   //const quantity = calculateQuantity(price);
+  //   await console.log(`купив по ${price}`);
   //   setBuyPrice(price);
   //   setMinPrice(null); // Reset the minPrice after buying
   // };
+
+  const handleBuy = async price => {
+    const quantity = calculateQuantity(price);
+    await placeOrder('BUY', price, quantity);
+    setBuyPrice(price);
+    setMinPrice(null); // Reset the minPrice after buying
+  };
 
   useEffect(() => {
     if (isMonitoring) {
